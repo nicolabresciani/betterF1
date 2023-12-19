489f250ecc5c94ef31b1859b7c4fe0d97a292094
nicolabresciani <101709282+nicolabresciani@users.noreply.github.com>
2023-12-19 11:42:18 +0100

    commit
    commit

---
+++ frontend/PaginaSottoAmministratore.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+// Verifica se l'utente è autenticato
+if (!isset($_SESSION["username"])) {
+    header("Location: Login.php");
+    exit();
+}
+
+
+$username = $_SESSION["username"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Benvenuto, <?php echo $username; ?>!</h1>
+    <p>benvenuto nella pagina riservata ai sotto Amministratori</p>
+    <a href="Logout.php">Logout</a>
+</body>
+</html>
